31b95059b7253b845e79c69828b9577e1274edd6
gleonato <gleonato@gmail.com>
2021-04-04 09:51:48 -0300

    Initialize s3read python

--- detect_from_s3_video.py
+++ detect_from_s3_video.py
@@ -20,8 +20,8 @@ import torch.nn as nn
 from PIL import Image as pil_image
 from tqdm import tqdm
 
-from FaceForensics.classification.network.models import model_selection
-from FaceForensics.classification.dataset.transform import xception_default_data_transforms
+from network.models import model_selection
+from dataset.transform import xception_default_data_transforms
 
 
 def get_boundingbox(face, width, height, scale=1.3, minsize=None):
