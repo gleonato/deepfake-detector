b482cabe59bb16cc0e1f10082132f21c3fba7169
gleonato <gleonato@gmail.com>
2021-04-04 07:42:23 -0300

    s3 read

---
+++ s3read.py
@@ -0,0 +1,11 @@
+import boto3
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('s3://raw-videos-gleonato/datasets/dfdc/deepfake-detection-challenge/test_videos')
+
+
+for obj in bucket.objects.all():
+    key = obj.key
+    print(key)
+    body = obj.get()['Body'].read()
+    print(body)
