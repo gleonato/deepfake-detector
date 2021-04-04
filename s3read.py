7cc7b6da37ce3913f62cd56abfad592def550682
gleonato <gleonato@gmail.com>
2021-04-04 08:50:36 -0300

    Initialize s3read python

--- s3read.py
+++ s3read.py
@@ -4,9 +4,9 @@ s3 = boto3.resource('s3')
 # bucket = s3.Bucket('raw-videos-gleonato/datasets/dfdc/deepfake-detection-challenge/test_videos/')
 bucket = s3.Bucket('raw-videos-gleonato')
 
-
 for obj in bucket.objects.all():
     key = obj.key
     print(key)
     body = obj.get()['Body'].read()
     print(body)
+
