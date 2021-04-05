2b20d4291278e269c996e34ad4703c07ae7bf09c
gleonato <gleonato@gmail.com>
2021-04-05 08:42:59 -0300

    updating...

--- detect_from_s3_video.py
+++ detect_from_s3_video.py
@@ -238,7 +238,7 @@ def test_full_image_network2(s3_bucket, model_path, output_path,
         key = obj.key
         print(key)
         # body = obj.get()['Body'].read()
-        s3.meta.client.download_file(bucket,key,key)
+        s3.meta.client.download_file(bucket,key,str(key))
         break
         # print(body)
 
